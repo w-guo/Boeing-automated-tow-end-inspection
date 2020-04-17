284cfab0b9275919bdad8459951c217328a23094
w-guo <weig@uw.edu>
2020-04-16 17:05:15 -0700

    Intial commit

---
+++ src/extract_lines.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.insert(0, 'C:\\Users\\BARC\\Desktop\\AFP\\binary_seg')
+
+import os, cv2, fnmatch
+import numpy as np
+from utils import lineMagnitude, find_major_orientations, merge_lines, remove_isolated_lines
+    
+h, w = (728, 968)
+img_h, img_w = (256, 256) 
+
+pad_h = (h//img_h+1)*img_h
+pad_w = (w//img_w+1)*img_w
+
+aug_h = (pad_h-h)/2
+aug_w = (pad_w-w)/2
+
+tol = 7
+cancat_range_x = []
+cancat_range_y = []
+for i in range(img_w, pad_w, img_w):
+    subimg_bd_x = int(i-aug_w)
+    range_x = range(subimg_bd_x-tol, subimg_bd_x+tol)
+    cancat_range_x += list(range_x)
+    
+for j in range(img_h, pad_h, img_h):
+    subimg_bd_y = int(j-aug_h)
+    range_y = range(subimg_bd_y-tol, subimg_bd_y+tol)
+    cancat_range_y += list(range_y)    
+ 
+subfolder ='unet_xception_resnet_nsgd32_lovasz/' 
+test_imgs = fnmatch.filter(os.listdir('./test/src/'), '*.jpg')
+
+for filename in test_imgs:
+    filename = test_imgs[85]
+    src = cv2.imread('./test/src/' + filename)
+    pred = cv2.imread('./test/results/'+ subfolder + os.path.splitext(filename)[0]+'.png', 0)
+    lsd = cv2.createLineSegmentDetector(0)   
+    # Detect lines in the image
+    lines = lsd.detect(pred)[0] # Position 0 of the returned tuple are the detected lines
+    filter_lines = []
+    for l in lines:
+        x1, y1, x2, y2 = l.flatten()
+        # discard the line segments at the boundary of sub-images
+        if abs(x2-x1) < 7 and int((x1+x2)/2) in cancat_range_x:
+            continue
+        if abs(y2-y1) < 7 and int((y1+y2)/2) in cancat_range_y:
+            continue
+        if  lineMagnitude(l.flatten()) > 20:  
+            # let the range of line oritations be [-90,90]
+            if x1 > x2:
+                x1,x2 = x2,x1
+                y1,y2 = y2,y1
+            filter_lines.append([x1, y1, x2, y2]) 
+            
+    filter_lines = find_major_orientations(filter_lines)  
+#    for fl in filter_lines:
+#        x1, y1, x2, y2 = np.rint(fl).astype(int)
+#        cv2.line(src, (x1, y1), (x2, y2), (0,0,255), 2, cv2.LINE_AA)
+#    cv2.imwrite('./test/line_extraction/' + os.path.splitext(filename)[0]+'_fo.png', src)       
+
+    filter_lines = merge_lines(filter_lines)
+#    for fl in filter_lines:
+#        x1, y1, x2, y2 = np.rint(fl).astype(int)
+#        cv2.line(src, (x1, y1), (x2, y2), (0,0,255), 2, cv2.LINE_AA)
+#    cv2.imwrite('./test/line_extraction/' + os.path.splitext(filename)[0]+'_fo_ml.png', src)
+    
+    filter_lines = remove_isolated_lines(filter_lines)  
+    for fl in filter_lines:
+        x1, y1, x2, y2  = np.rint(fl).astype(int)    
+        cv2.line(src, (x1, y1), (x2, y2), (0,0,255), 2, cv2.LINE_AA)
+    cv2.imwrite('./test/line_extraction/'+ os.path.splitext(filename)[0]+'_fo_ml_rm.png', src) 
---
+++ src/utils/gen_img_aug.py
@@ -0,0 +1,166 @@
+import os, cv2, fnmatch, random
+import numpy as np
+from shutil import copy2
+from sklearn.model_selection import train_test_split
+
+np.random.seed(2018)
+
+img_w, img_h = (256, 256) 
+w, h = (968, 728)
+folder = './aug_100k/'
+
+def scale(src,label,factor):
+    #scale in: scale image by given factor < 1
+    #scale out: scale image by given factor > 1
+    dst_img = cv2.resize(src,None,fx = factor, fy = factor, interpolation = cv2.INTER_LINEAR)
+    dst_label = cv2.resize(label,None,fx = factor, fy = factor, interpolation = cv2.INTER_LINEAR)
+    return dst_img, dst_label
+
+def chooseScale(src, label, randTemp):
+    if randTemp < 1/2:
+        #scale in
+        begin_w = random.randint(0, w-2*img_w-1)
+        begin_h = random.randint(0, h-2*img_h-1)
+        src_sub = src[begin_h: begin_h + 2*img_h, begin_w: begin_w+ 2*img_w]
+        label_sub = label[begin_h: begin_h + 2*img_h, begin_w: begin_w+ 2*img_w]
+        src_sub_l1, label_sub_l1 = scale(src_sub, label_sub, 0.5) 
+    else:
+#    elif 1/3 <= randTemp < 2/3:
+        #no scale
+        begin_w = random.randint(0, w-img_w-1)
+        begin_h = random.randint(0, h-img_h-1)
+        src_sub_l1 = src[begin_h: begin_h + img_h, begin_w: begin_w+img_w]
+        label_sub_l1 = label[begin_h: begin_h + img_h, begin_w: begin_w+img_w]
+#    else:
+#        #scale out
+#        begin_w = random.randint(0, w-0.5*img_w-1)
+#        begin_h = random.randint(0, h-0.5*img_h-1)
+#        src_sub = src[begin_h: int(begin_h + 0.5*img_h), begin_w: int(begin_w+ 0.5*img_w)]
+#        label_sub = label[begin_h: int(begin_h + 0.5*img_h), begin_w: int(begin_w+0.5*img_w)]
+#        src_sub_l1, label_sub_l1 = scale(src_sub, label_sub, 2)             
+    return src_sub_l1, label_sub_l1
+    
+def chooseRotate(src_sub_l1, label_sub_l1, randTemp):
+    
+    src_sub_r90 = np.rot90(src_sub_l1)
+    label_sub_r90 = np.rot90(label_sub_l1)
+    if randTemp < 1/8:
+        src_sub_l2 = src_sub_l1
+        label_sub_l2 = label_sub_l1
+    elif 1/8 <= randTemp < 2/8:
+        # rotate 90 degrees
+        src_sub_l2 = src_sub_r90
+        label_sub_l2 = label_sub_r90
+    elif 2/8 <= randTemp < 3/8:
+        # rotate 180 degrees
+        src_sub_l2 = np.rot90(src_sub_l1, 2)
+        label_sub_l2 = np.rot90(label_sub_l1, 2)      
+    elif 3/8 <= randTemp < 4/8:
+        # rotate 270 degrees
+        src_sub_l2 = np.rot90(src_sub_l1, 3)
+        label_sub_l2 = np.rot90(label_sub_l1, 3)      
+    elif 4/8 <= randTemp < 5/8:
+        # flip image horizontally
+        src_sub_l2 = cv2.flip(src_sub_l1, 0)
+        label_sub_l2 = cv2.flip(label_sub_l1, 0)    
+    elif 5/8 <= randTemp < 6/8:
+        # flip image vertically
+        src_sub_l2 = cv2.flip(src_sub_l1, 1)
+        label_sub_l2 = cv2.flip(label_sub_l1, 1)      
+    elif 6/8 <= randTemp < 7/8:
+        # flip image horizontally
+        src_sub_l2 = cv2.flip(src_sub_r90, 0)
+        label_sub_l2 = cv2.flip(label_sub_r90, 0)   
+    else:
+        # flip image vertically
+        src_sub_l2 = cv2.flip(src_sub_r90, 1)
+        label_sub_l2 = cv2.flip(label_sub_r90, 1)  
+
+    return src_sub_l2, label_sub_l2
+
+def chooseEquHist(src_sub_l1, randTemp):
+    if randTemp < 0.5:
+        src_sub_l2 = cv2.equalizeHist(np.array(src_sub_l1, dtype = np.uint8))
+    else:
+        src_sub_l2 = src_sub_l1
+    return src_sub_l2
+  
+    
+gamma = 1.2   
+invGamma = 1.0 / gamma
+table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    
+def chooseGamma(src_sub_l1, randTemp):
+    if randTemp < 0.5:
+        src_sub_l2 = cv2.LUT(np.array(src_sub_l1, dtype = np.uint8), table)
+    else:
+        src_sub_l2 = src_sub_l1
+    return src_sub_l2
+    
+def chooseGaussNoise(src_sub_l1, randTemp):
+    if randTemp < 0.5:
+       gauss = src_sub_l1.copy()    
+       cv2.randn(gauss, 0, 1)
+       src_sub_l2 = src_sub_l1 + gauss
+    else:
+        src_sub_l2 = src_sub_l1
+    return src_sub_l2
+
+def chooseSPNoise(src_sub_l1, randTemp):
+    src_sub_l2 = src_sub_l1.copy()
+    salt_vs_pepper = 0.5
+    amount = 0.004
+    num_salt = np.ceil(amount * src_sub_l2.size * salt_vs_pepper)
+    num_pepper = np.ceil(amount * src_sub_l2.size * (1.0 - salt_vs_pepper))
+    
+    if randTemp < 1/2:
+        # Add Salt noise
+        coords = [np.random.randint(0, i - 1, int(num_salt)) for i in src_sub_l2.shape]
+        src_sub_l2[coords[0], coords[1]] = 255
+        # Add Pepper noise
+        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in src_sub_l2.shape]
+        src_sub_l2[coords[0], coords[1]] = 0      
+    else:
+        src_sub_l2 = src_sub_l1
+    return src_sub_l2
+
+image_files = fnmatch.filter(os.listdir('./data/src'), '*.jpg') 
+train_set, test_set = train_test_split(image_files, test_size=0.1, random_state=2018)  
+
+for filename in test_set:
+    copy2('./data/src/'+filename, folder+'test/src')
+    copy2('./data/label/'+filename, folder+'test/label')
+
+image_num = 100000
+image_each = image_num / len(train_set)
+
+for filename in train_set:
+    count = 1
+    src = cv2.imread('./data/src/'+filename, 0)
+    label = cv2.imread('./data/label/'+filename, 0)
+    while count < image_each:
+        #temp1 = np.random.random()
+        temp2 = np.random.random()
+#        temp3 = np.random.random()
+#        temp4 = np.random.random()
+#        temp5 = np.random.random()
+        #scaling
+        src_sub, label_sub = chooseScale(src, label, 1)
+        #rotate/flip
+        src_sub, label_sub = chooseRotate(src_sub, label_sub, temp2)
+        #apply equ hist
+        #src_sub = chooseEquHist(src_sub, temp3)
+        #apply gamma correction
+        #src_sub = chooseGamma(src_sub, temp4)
+        #add Gaussian noise
+        #src_sub = chooseGaussNoise(src_sub temp5)
+        #add salt & pepper noise
+        #src_sub = chooseSPNoise(src_sub, temp5)
+        cv2.imwrite((folder+'train/src/'+filename+'_%d.jpg' % count), src_sub)
+        if np.sum(np.rint(label_sub/255)) == img_w*img_h:
+            label_sub = np.zeros_like(label_sub)
+        cv2.imwrite((folder+'train/label/'+filename+'_%d.jpg' % count), label_sub)        
+        count += 1 
+        
+        
+        
